efbe5a78288b0d40f9145a3c5344c5d579a68d12
SethTouchet26 <001014893@tarleton.edu>
2026-03-24 12:04:01 -0500

    HW-18.cu

---
+++ HW-18.cu
@@ -0,0 +1,81 @@
+// Name: Seth Touchet
+// GPU random walk. 
+// nvcc P_GPURandomWalk.cu -o temp
+
+/*
+ What to do:
+ This code runs a random walk for 10,000 steps on the CPU.
+
+ 1. Use cuRAND to run 2,000 random walks of 10,000 steps simultaneously on the GPU, each with a different seed.
+    Print the final positions of random walks 5, 100, 789, and 1622 as a spot check to get a warm 
+    fuzzy feeling that your code is producing different random walks for each thread.
+
+ 2. Use cudaMallocManaged(&variable, amount_of_memory_needed);
+    This allocates unified memory, which is automatically managed between the CPU and GPU.
+    You lose some control over placement, but it saves you from having to manually copy data
+    to and from the GPU.
+*/
+
+/*
+ Purpose:
+ To learn how to use cuRAND and unified memory.
+*/
+
+/*
+ Note:
+ The maximum signed int value is 2,147,483,647, so the maximum unsigned int value is 4,294,967,295.
+
+ RAND_MAX is guaranteed to be at least 32,767. When I checked it on my laptop (10/6/2025), it was 2,147,483,647.
+ rand() returns a value in [0, RAND_MAX]. It actually generates a list of pseudo-random numbers that depends on the seed.
+ This list eventually repeats (this is called its period). The period is usually 2³¹ = 2,147,483,648,
+ but it may vary by implementation.
+
+ Because RAND_MAX is odd on this machine and 0 is included, there is no exact middle integer.
+ Casting to float as in (float)RAND_MAX / 2.0 divides the range evenly.
+ Using integer division (RAND_MAX / 2) would bias results slightly toward the positive side by one value out of 2,147,483,647.
+
+ I know this is splitting hares (sorry, rabbits), but I'm just trying to be as accurate as possible.
+ You might do this faster with a clever integer approach, but I’m using floats here for clarity.
+*/
+
+// Include files
+#include <sys/time.h>
+#include <stdio.h>
+
+// Defines
+
+// Globals
+int NumberOfRandomSteps = 10000;
+float MidPoint = (float)RAND_MAX/2.0f;
+
+// Function prototypes
+int getRandomDirection();
+int main(int, char**);
+
+int getRandomDirection()
+{	
+	int randomNumber = rand();
+	
+	if(randomNumber < MidPoint) return(-1);
+	else return(1);
+}
+
+int main(int argc, char** argv)
+{
+	srand(time(NULL));
+	
+	printf(" RAND_MAX for this implementation is = %d \n", RAND_MAX);
+	
+	int positionX = 0;
+	int positionY = 0;
+	for(int i = 0; i < NumberOfRandomSteps; i++)
+	{
+		positionX += getRandomDirection();
+		positionY += getRandomDirection();
+	}
+	
+	printf("\n Final position = (%d,%d) \n", positionX, positionY);
+	return 0;
+}
+
+
